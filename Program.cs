7b5dff95ff1320eca743941ef9c96c9f137e2553
rats420 <123443334+rats420@users.noreply.github.com>
2023-01-27 08:57:50 +0200

    Pirma augsupielade

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RA_27._01._2023.VS._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
